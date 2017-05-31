6e38a9f4070aa3086afb286bee1a35b2ddecd685
piramidex <piramidex@gmail.com>
2017-05-30 22:26:25 -0400

    Update backend to handle insertion of variants generated in frontend

--- Classes/Hooks/class.tx_msvariants_insertproductposthook.php
+++ Classes/Hooks/class.tx_msvariants_insertproductposthook.php
@@ -3,140 +3,163 @@
 
 class tx_msvariants_insertproductposthook {
 
-  function insertProductPostHook(&$params, &$reference) {
+  private $attribute_id = array();
+  private $option_id = array();
+  private $option_value_id = array();
 
 
-    // First time product is created so variants must be generated
-    // and updated with the form inputs.....
-    // TODO update with the form inputs
+  function insertProductPostHook(&$params, &$ref) {
+    error_log("insertProductPostHook - begin");
 
-    error_log("insertProductPostHook - begin!");
+    $product_id = $params['products_id'];
+    $this->init($product_id);
 
-    $products_id = $params['products_id'];
+    foreach($this->variantsFromPost($ref->post) as $v) {
+      $variant_id = $this->insertVariantInDb($v, $product_id);
 
-    error_log("products_id: ".$params['products_id']);
-    error_log("products_id: ".$products_id);
+      foreach($v['attributes'] as $a) {
 
-    // product Attribute
-    //if (!$reference->ms['MODULES']['DISABLE_PRODUCT_VARIANTS_TAB_IN_EDITOR'])
+        $this->insertVariantAttributeInDb(
+          $variant_id, $product_id,
+          $this->option_id($a['option_name_or_id']),
+          $this->option_value_id($a['option_value_name_or_id']));
+      }
+    }
 
-
-    // Get attributes (option and options values) for product from db
-    // and create variants based on attributes and insert them into db
-
-    $output = '';
-
-        $sql_pa=$GLOBALS ['TYPO3_DB']->SELECTquery('popt.required,popt.products_options_id, popt.products_options_name, popt.listtype, patrib.*', // SELECT ...
-          'tx_multishop_products_options popt, tx_multishop_products_attributes patrib', // FROM ...
-          "patrib.products_id='".$products_id."' and popt.language_id = '0' and patrib.options_id = popt.products_options_id", // WHERE.
-          '', // GROUP BY...
-          'patrib.sort_order_option_name, patrib.sort_order_option_value', // ORDER BY...
-          '' // LIMIT ...
-        );
-
-        $qry_pa=$GLOBALS ['TYPO3_DB']->sql_query($sql_pa);
-
-        if ($products_id) {
-
-          if ($GLOBALS['TYPO3_DB']->sql_num_rows($qry_pa)>0) {
-
-            $ctr=1;
-            $options_data=array();
-            $attributes_data=array();
-            while (($row=$GLOBALS ['TYPO3_DB']->sql_fetch_assoc($qry_pa))!=false) {
-              $row['options_values_name']=mslib_fe::getNameOptions($row['options_values_id']);
-              $options_data[$row['products_options_id']]=$row['products_options_name'];
-              $attributes_data[$row['products_options_id']][]=$row;
-            }
-
-            if (count($options_data)) {
+    error_log("insertProductPostHook - end");
+  }
 
 
-              $num_options = count($options_data);
-              $options_ids = array_keys($options_data);
-              $counters = array_fill(0, $num_options, 0);
+  private function variantsFromPost($post) {
+    error_log("variantsFromPost() called");
 
-              $limits = array();
-              $k = 0;
-              foreach ($options_ids as $option_id) {
-                $limits[$k] = count($attributes_data[$option_id])-1;
-                $k++;
-              }
+    $variants = array();
 
-              $content_variants = '';
+    $variants_ids = $post['variants_ids'];
+    $variants_prices = $post['variants_prices'];
+    $variants_stocks = $post['variants_stocks'];
+    $variants_skus = $post['variants_skus'];
+    $combinations = $post['combinations'];
 
-              $k = 0;
-              while ($k < $num_options) {
+    for($i=0; $i < count($variants_ids); $i++) {
 
-                if ($k == 0) {
+      $attributes = array();
+      foreach(explode('||', $combinations[$i]) as $c) {
+        $temp = explode('|', $c);
+        $attr = array();
+        $attr['option_name_or_id'] = $temp[0];
+        $attr['option_value_name_or_id'] = $temp[1];
+        $attributes[] = $attr;
+      }
 
-                  $variant_name = 'Variant: ';
-                  $content_options = '';
-                  $markerArray = array();
+      $variants[] = array(
+        'id' => $variants_ids[$i],
+        'price' => $variants_prices[$i],
+        'stock' => $variants_stocks[$i],
+        'sku' => $variants_skus[$i],
+        'attributes' => $attributes
+      );
+    }
 
-                  // insert (variant_id, product_id, price, stock, sku) into db
-                  $variant_row = array(
-                    'variant_id' => 0,
-                    'product_id' => $products_id,
-                    'variant_price' => 0.0,
-                    'variant_stock' => 0,
-                    'variant_sku' => '',
-                  );
+    error_log("variants: ".print_r($variants, true));
+
+    return $variants;
+  }
 
 
-                  $res = $GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_msvariants_domain_model_variants', $variant_row);
-                  error_log('variant row inserted res; '.$res);
+  private function insertVariantInDb($variant, $product_id) {
+    error_log('insertVariantInDb() called');
 
-                  // TODO there must be a better way to set the variant_id or rename uid
-                  $variant_id = $GLOBALS['TYPO3_DB']->sql_insert_id();
-                  $update_array = array( 'variant_id' => $variant_id);
-                  $res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery(
-                    'tx_msvariants_domain_model_variants',
-                    'uid='.$variant_id, // TODO make this secure with fullQuoteStr
-                    $update_array
-                  );
+    $insert_array = array(
+      'variant_id' => 0,
+      'product_id' => $product_id,
+      'variant_price' => $variant['price'],
+      'variant_stock' => $variant['stock'],
+      'variant_sku' => $variant['sku']);
+    error_log('insert_array: '.print_r($insert_array, true));
 
-                  // insert list of options, values for the variant into db
-                  for($i = ($num_options-1); $i >= 0; $i--) {
+    $res = $GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_msvariants_domain_model_variants', $insert_array);
+    error_log('variant row inserted res: '.$res);
 
-                    $attribute_id = $attributes_data[$options_ids[$i]][$counters[$i]]['products_attributes_id'];
-                    $option_id = $attributes_data[$options_ids[$i]][$counters[$i]]['options_id'];
-                    $option_value_id = $attributes_data[$options_ids[$i]][$counters[$i]]['options_values_id'];
-                    $option_name = $attributes_data[$options_ids[$i]][$counters[$i]]['products_options_name'];
-                    $option_value = $attributes_data[$options_ids[$i]][$counters[$i]]['options_values_name'];
-                    $variant_name .= $attributes_data[$options_ids[$i]][$counters[$i]]['options_values_name']." - ";
+    $variant_id = $GLOBALS['TYPO3_DB']->sql_insert_id();
+    $update_array = array( 'variant_id' => $variant_id);
+    $res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+      'tx_msvariants_domain_model_variants',
+      'uid='.$variant_id, // TODO make this secure with fullQuoteStr
+       $update_array);
+    error_log('variant id updated res: '.$res);
 
-                    // insert (variant_id, ..., option, value) into db
-                    $variant_option_row = array(
-                      'variant_id' => $variant_id,
-                      'product_id' => $products_id,
-                      'attribute_id' => $attribute_id,
-                      'option_id' => $option_id,
-                      'option_value_id' => $option_value_id,
-                    );
-                    $res = $GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_msvariants_domain_model_variantsattributes', $variant_option_row);
-                    error_log('variantattribute row inserted res; '.$res);
-                  }
-                }
-
-                $counters[$k]++;
-
-                if ($counters[$k] > $limits[$k]) {
-                  $counters[$k] = 0;
-                  $k++;
-                }
-                elseif ($k > 0) {
-                  $k = 0;
-                }
-              }
-            }
-          }
-        }
-
-        error_log("insertProductPostHook - end");
+    return $variant_id;
 
   }
 
+
+  private function insertVariantAttributeInDb($variant_id, $product_id, $option_id, $option_value_id) {
+    error_log('insertVariantAttributeInDb() called');
+
+    $insert_array = array(
+      'variant_id' => $variant_id,
+      'product_id' => $product_id,
+      'attribute_id' => $this->attribute_id($option_id, $option_value_id),
+      'option_id' => $option_id,
+      'option_value_id' => $option_value_id);
+    error_log('insert_array: '.print_r($insert_array, true));
+
+    $res = $GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_msvariants_domain_model_variantsattributes', $insert_array);
+    error_log('variant attribute row inserted res: '.$res);
+  }
+
+
+  private function attribute_id($option_id, $option_value_id) {
+    error_log('attribute_id() called');
+    return $this->attribute_id[$option_id.'|'.$option_value_id];
+  }
+
+
+  private function option_id($name_or_id) {
+    if (isset($this->$option_id[$name_or_id])) {
+      return $this->$option_id[$name_or_id];
+    }
+    else {
+      return $name_or_id;
+    }
+  }
+
+
+  private function option_value_id($name_or_id) {
+    if (isset($this->$option_value_id[$name_or_id])) {
+      return $this->option_value_id[$name_or_id];
+    }
+    else {
+      return $name_or_id;
+    }
+  }
+
+
+  private function init($product_id) {
+    error_log('init() called');
+
+    $sql_pa=$GLOBALS ['TYPO3_DB']->SELECTquery(
+      'popt.required,popt.products_options_id, popt.products_options_name, popt.listtype, patrib.*', // SELECT ...
+      'tx_multishop_products_options popt, tx_multishop_products_attributes patrib', // FROM ...
+      "patrib.products_id='".$product_id."' and popt.language_id = '0' and patrib.options_id = popt.products_options_id", // WHERE.
+      '', // GROUP BY...
+      'patrib.sort_order_option_name, patrib.sort_order_option_value', // ORDER BY...
+      ''); // LIMIT ...
+
+    $qry_pa=$GLOBALS ['TYPO3_DB']->sql_query($sql_pa);
+    if ($GLOBALS['TYPO3_DB']->sql_num_rows($qry_pa)>0) {
+        while (($row=$GLOBALS ['TYPO3_DB']->sql_fetch_assoc($qry_pa))!=false) {
+          $this->attribute_id[$row['products_options_id'].'|'.$row['options_values_id']] = $row['products_attributes_id'];
+          $this->option_id[$row['products_options_name']] = $row['products_options_id'];
+          $this->option_value_id[$row['options_values_id']] = mslib_fe::getNameOptions($row['options_values_id']);
+        }
+    }
+
+    error_log('$this->attribute_id: '.print_r($this->attribute_id, true));
+    error_log('$this->option_id: '.print_r($this->option_id, true));
+    error_log('$this->option_value_id: '.print_r($this->option_value_id, true));
+  }
 
 }
 
